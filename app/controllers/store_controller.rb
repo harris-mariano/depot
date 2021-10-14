cbf7f7cb427d8563751b510e8f6a4007ed2980f2
harris-mariano <harris.mariano@adish.co.jp>
2021-10-14 17:13:17 +0800

    Added Add to cart and AJAX

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  before_action :count_session_visit
+
   def index
     @products = Product.order(:title)
+  end
+
+  private
+
+  def count_session_visit
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211014015118) do
+ActiveRecord::Schema.define(version: 20211014063223) do
+
+  create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci" do |t|
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci" do |t|
     t.string "title"
@@ -21,4 +36,6 @@ ActiveRecord::Schema.define(version: 20211014015118) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
